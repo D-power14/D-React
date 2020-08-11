ea718b8ca4815a6ba78ef710e25df6dd6edaafbd
Dn12 <62524139+D-power14@users.noreply.github.com>
2020-08-11 10:13:50 -0400

    actualizando page

---
+++ paginatest/src/App.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import logo from './assets/images/logo.svg';
+import './assets/css/App.css';
+
+//importando componentes
+
+import MiComponente from './components/Mi componente.js';
+
+
+function HolaMundo(hola) {
+  var hola = (
+    <div> 
+    <h2 className="Holi">Hola mundo</h2>
+    <h3>Hola mundo!!!</h3>
+  </div>
+  )
+  return hola
+}
+
+function Footer(footer) {
+  var footer = ( 
+    <footer className="footer"><p> pie de pagina</p></footer >
+  )
+  return footer
+}
+
+function App() {
+  var hola = " "
+  
+  
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hola a tu, Bienvenido a este paso en mi carrera
+        </p>
+        
+        {HolaMundo(hola)}
+        
+      </header>
+      
+      <section className="componentes">
+        <MiComponente/>
+        <MiComponente/>
+        <MiComponente/>
+        <MiComponente/>
+      </section>
+    </div>
+  );
+}
+
+export default App;
+
+
---
+++ paginatest/src/components/Mi componente.js
@@ -0,0 +1,18 @@
+
+import React,{Component}from 'react';
+
+class MiComponente extends React.Component{
+
+     render(){
+          return (
+               <React.Fragment>
+                    <h1> Hola, soy el componente llamado: Mi componente </h1>
+                    <h2>Estoy testeando</h2>
+                    <hr/>
+               </React.Fragment>
+          
+          );
+     }
+}
+
+export default MiComponente;
